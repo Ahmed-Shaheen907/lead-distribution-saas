9b3717b5a30e781840b06b7997618cc2137aea12
Ahmed Shaheen <ahmedshaheen13802@gmail.com>
2025-12-03 18:05:20 +0200

    Fix Supabase env variables

--- app/api/incoming-lead/route.js
+++ app/api/incoming-lead/route.js
@@ -1,27 +1,35 @@
+export const runtime = "nodejs";
+
 export async function POST(request) {
-    const expectedToken = process.env.TELEGRAM_BOT_TOKEN;
+    try {
+        const body = await request.json();
 
-    if (!expectedToken) {
-        return new Response(JSON.stringify({ error: "ENV NOT LOADED" }), {
-            status: 500,
-        });
+        const expectedToken = process.env.TELEGRAM_BOT_TOKEN;
+
+        console.log(expectedToken);
+        console.log('niggagasgagsgasg');
+
+        if (!expectedToken) {
+            return new Response(JSON.stringify({ error: "telegramBotToken is NOT configured" }), {
+                status: 500,
+            });
+        }
+
+        // Check header
+        const incomingToken = request.headers.get("X-Incoming-Token");
+
+        if (incomingToken !== expectedToken) {
+            return new Response(JSON.stringify({ error: "invalid token" }), {
+                status: 401,
+            });
+        }
+
+        console.log("🔥 Incoming lead received:", body);
+
+        return new Response(JSON.stringify({ success: true }), { status: 200 });
+
+    } catch (err) {
+        console.error(err);
+        return new Response(JSON.stringify({ error: "server error" }), { status: 500 });
     }
-
-    const incomingToken = request.headers.get("X-Incoming-Token");
-
-    if (!incomingToken) {
-        return new Response(JSON.stringify({ error: "Missing token" }), {
-            status: 400,
-        });
-    }
-
-    if (incomingToken !== expectedToken) {
-        return new Response(JSON.stringify({ error: "Invalid token" }), {
-            status: 401,
-        });
-    }
-
-    return new Response(JSON.stringify({ success: true }), {
-        status: 200,
-    });
 }
