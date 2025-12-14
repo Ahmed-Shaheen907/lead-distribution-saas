966374b24baa5b5df9125e549eed3a12d9a31486
Ahmed Shaheen <ahmedshaheen13802@gmail.com>
2025-12-15 00:36:06 +0200

    fix job

--- app/logs/page.jsx
+++ app/logs/page.jsx
@@ -191,7 +191,7 @@ export default function LeadLogsPage() {
               <strong>Phone:</strong> {log.lead_json?.phone}
             </p>
             <p>
-              <strong>Job:</strong> {log.lead_json?.job}
+              <strong>Job:</strong> {log.lead_json?.job_title}
             </p>
             <p>
               <strong>Description:</strong> {log.lead_json?.description}
